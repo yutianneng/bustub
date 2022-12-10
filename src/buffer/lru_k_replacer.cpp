5b4271169fb1b2d46eaf22a0bcd1fa92f0b96d4d
yutianneng <yutianneng@outlook.com>
2022-12-10 20:53:21 +0800

    lru-k-replacer

--- src/buffer/lru_k_replacer.cpp
+++ src/buffer/lru_k_replacer.cpp
@@ -14,16 +14,125 @@
 
 namespace bustub {
 
-LRUKReplacer::LRUKReplacer(size_t num_frames, size_t k) : replacer_size_(num_frames), k_(k) {}
+LRUKReplacer::LRUKReplacer(size_t num_frames, size_t k)
+    : replacer_size_(num_frames), k_(k) {}
 
-auto LRUKReplacer::Evict(frame_id_t *frame_id) -> bool { return false; }
+auto LRUKReplacer::KDistance(frame_id_t frame_id) -> int {
+    auto frameinfo = frameinfo_map.find(frame_id);
+    if (frameinfo == frameinfo_map.end()) {
+        return -1;
+    }
+    if (frameinfo->second->GetHistory().size() < k_) {
+        return INT32_MAX;
+    }
+    // 最后一个是最新的访问时间，第一个是最旧的访问时间
+    return *frameinfo->second->GetHistory().rbegin() -
+           (*frameinfo->second->GetHistory().begin());
+}
 
-void LRUKReplacer::RecordAccess(frame_id_t frame_id) {}
+auto LRUKReplacer::Evict(frame_id_t* frame_id) -> bool {
+    std::scoped_lock<std::mutex> lock(latch_);
+    if (curr_size_ == 0)
+        return false;
+    int k_distance = 0;
+    auto iter = frameinfo_list.begin();
+    auto evict_iter = frameinfo_list.end();
+    for (; iter != frameinfo_list.end(); ++iter) {
+        // 过滤不可淘汰帧
+        if (!iter->get()->GetEvictable()) {
+            continue;
+        }
+        int kdis = KDistance(iter->get()->GetFrameID());
+        // 头部是最久访问的，尾部是最近访问的，如果都为INT32_MAX，那优先淘汰掉最久访问
+        if (evict_iter == frameinfo_list.end()) {
+            evict_iter = iter;
+            k_distance = kdis;
+        } else if (kdis > k_distance) {
+            // 加入当前帧k距离不是INT32_MAX
+            //      1. 如果evict_iter==
+            //      INT32_MAX，那么就选择当前帧，优先淘汰满了k次访问的；
+            //      2. 如果kdis > k_distance，优先淘汰距离时间最长的
+            evict_iter = iter;
+            k_distance = kdis;
+        }
+    }
+    *frame_id = evict_iter->get()->GetFrameID();
+    frameinfo_list.erase(evict_iter);
+    frameinfo_map.erase(*frame_id);
+    curr_size_--;
+    return true;
+}
+// 在pinned之后使用，会加入new frame
+void LRUKReplacer::RecordAccess(frame_id_t frame_id) {
+    std::scoped_lock<std::mutex> lock(latch_);
+    // replacer_size_等于buffer_pool_size，frame_id从0开始
+    BUSTUB_ASSERT(size_t(frame_id) < replacer_size_, "frame_id invalid!!!");
+    auto iter = frameinfo_map.find(frame_id);
+    // 如果命中，直接更新状态即可
+    if (iter != frameinfo_map.end()) {
+        if (iter->second->GetHistory().size() == k_) {
+            iter->second->GetHistory().pop_front();
+        }
+        // list尾部是最近访问时间
+        iter->second->GetHistory().push_back(ticks++);
+        // 放到尾部，表示最近访问的，优先不淘汰
+        frameinfo_list.erase(iter->second->GetIter());
+        frameinfo_list.push_back(iter->second);
+        iter->second->SetIter(--frameinfo_list.end());
+        return;
+    }
+    // 增加新帧
+    auto frameinfo = std::make_shared<FrameInfo>();
+    frameinfo->GetHistory().push_back(ticks++);
+    frameinfo->SetFrameID(frame_id);
 
-void LRUKReplacer::SetEvictable(frame_id_t frame_id, bool set_evictable) {}
+    frameinfo_list.push_back(frameinfo);
+    frameinfo->SetIter(--frameinfo_list.end());
+    frameinfo_map[frame_id] = frameinfo;
+}
 
-void LRUKReplacer::Remove(frame_id_t frame_id) {}
+void LRUKReplacer::SetEvictable(frame_id_t frame_id, bool set_evictable) {
+    std::scoped_lock<std::mutex> lock(latch_);
+    // 会影响可淘汰帧的数量
+    BUSTUB_ASSERT(size_t(frame_id) < replacer_size_, "frame_id is invalid!!!");
+    auto iter = frameinfo_map.find(frame_id);
+    BUSTUB_ASSERT(iter != frameinfo_map.end(), "frame id not found!!!");
 
-auto LRUKReplacer::Size() -> size_t { return 0; }
+    auto frameinfo = iter->second;
+    // 如果该frame原本是可淘汰的，但现在设置为不可淘汰
+    if (frameinfo->GetEvictable() && !set_evictable) {
+        frameinfo->SetEvictable(set_evictable);
+        curr_size_--;
+    } else if (!frameinfo->GetEvictable() && set_evictable) {
+        // 原本不可淘汰，现在设置为可淘汰
+        frameinfo->SetEvictable(set_evictable);
+        curr_size_++;
+        // 不修改位置
+        //  frameinfo_list.erase(frameinfo->GetIter());
+        //  frameinfo_list.push_back(frameinfo);
+        //  frameinfo->SetIter(--frameinfo_list.end());
+    }
+}
+
+void LRUKReplacer::Remove(frame_id_t frame_id) {
+    std::scoped_lock<std::mutex> lock(latch_);
+
+    BUSTUB_ASSERT(size_t(frame_id) < replacer_size_, "frame_id is invalid!!!");
+    auto iter = frameinfo_map.find(frame_id);
+    if (iter == frameinfo_map.end()) {
+        return;
+    }
+    BUSTUB_ASSERT(iter->second->GetEvictable(), "the frame not evictable!!!");
+    frameinfo_list.erase(iter->second->GetIter());
+    frameinfo_map.erase(iter);
+    curr_size_--;
+}
+
+auto LRUKReplacer::Size() -> size_t {
+    return curr_size_;
+}
+
+LRUKReplacer::FrameInfo::FrameInfo(frame_id_t frame_id)
+    : frame_id{frame_id}, evictable{false} {}
 
 }  // namespace bustub
